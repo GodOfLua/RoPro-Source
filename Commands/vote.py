c319ccc898cfdc45ea003c14d687144a01bb8ae7
snoopy13124 <46417021+snoopy13124@users.noreply.github.com>
2021-03-14 13:15:11 +0100

    Removed setup command

---
+++ Commands/vote.py
@@ -0,0 +1,12 @@
+
+import json 
+import asyncio 
+import discord 
+import requests
+from discord.utils import get
+from Modules.Discord.Embeds import *
+from Modules.DataManagement import *
+from Modules.Discord.Author import *
+import secrets
+
+async def run(message, Arguments, Client, Discord_Bot):
