ee5bc11f21b7d6915eea05923bb0bccfe1710f39
GodOfLua <46417021+GodOfLua@users.noreply.github.com>
2021-09-11 18:52:58 +0200

    Update Bot.py

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -16,15 +16,6 @@ from discord.utils import get
 async def status(Client):
     import asyncio
     while True:
-        #x = requests.post("https://top.gg/api//bots/810478441224732702/stats", data = json.dumps({
-            #"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxMDQ3ODQ0MTIyNDczMjcwMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjE1MzcxNTcwfQ.vb0pnlkBUVW_JllUPUIazki25BF8eakvMdTlJ4kDqo8",
-            #"server_count": len(list(Client.guilds))
-        #}), headers={'content-type': 'application/json'})
-        #print(x.status_code)
-        #try:
-            #print(x.text)
-        #except:
-            #pass
         await Client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=" ! | !invite"))
         await asyncio.sleep(20)
         totalCount = 0
@@ -135,7 +126,7 @@ class Bot():
         Token = Settings["TOKEN"]
 
         if TestMode:
-            Token = "NzA3NTkyNzkxMjI3NjI5NjQy.XrLDIw.vCAYlAw9t1ku8emJ_0IfhVWejfg"
+            Token = ""
         return Token 
 
     def Client(self):
